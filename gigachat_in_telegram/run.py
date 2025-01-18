c86a8717c63a67d54949a6ce9fa9f10a530e5f26
Tim <you@example.com>
2025-01-18 16:25:08 +0300

    tg bot fix

--- gigachat_in_telegram/run.py
+++ gigachat_in_telegram/run.py
@@ -5,17 +5,19 @@ from gigachat_in_telegram import ai
 
 load_dotenv()
 
-
 bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
 
 
 @bot.message_handler(commands=["help", "start"])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "я гигачат")
+    bot.send_message(
+        message.chat.id,
+        """я гигачат\nРепозиторий: github.com/w1ghton/gigachat-in-telegram""",
+    )
 
 
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
+def gigachat_answer(message):
     bot.reply_to(message, ai.generate_text(message.text))
 
 
