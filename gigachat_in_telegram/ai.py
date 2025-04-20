e2634a0841ed3fcf94d0503ea7fd47012ecbc7d4
Тимофей Кушаков <github.smashup586@passinbox.com>
2025-04-21 00:41:27 +0300

    Update ai.py

--- gigachat_in_telegram/ai.py
+++ gigachat_in_telegram/ai.py
@@ -1,20 +1,8 @@
-import re
 from env import Settings
 from gigachat import GigaChat
 
 
-def escape_markdown_v2(text: str) -> str:
-    return re.sub(r"([\[\]()>#+\-=|{}.!])", r"\\\1", text)
-
-
 def generate_text(query: str) -> str:
-    FORMAT_TEXT = """При необходимости форматирования используй это:
-                    **Жирный текст**
-                    __Текст курсивом__
-                    ~~Зачеркнутый текст~~
-                    `Моноширинный текст`
-                    ```Несколько строк кода```"""
-
     model = GigaChat(
         credentials=Settings().gigachat_api_key,
         model="GigaChat",
@@ -24,10 +12,10 @@ def generate_text(query: str) -> str:
     response = model.chat(
         payload={
             "messages": [
-                {"role": "system", "content": FORMAT_TEXT},
+                {"role": "system", "content": "Не используй форматирование"},
                 {"role": "user", "content": query},
             ],
             "max_tokens": 512,
         }
     )
-    return escape_markdown_v2(response.choices[0].message.content)
+    return response.choices[0].message.content
